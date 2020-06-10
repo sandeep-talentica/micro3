0e1a6292b6ad464e4323f26076c17cdb13578c1a
Sandeep <dhalosandeepthakur@gmail.com>
2020-06-11 01:26:15 +0530

    First Commit

---
+++ src/main/java/com/sandeep/ws/Micro3Application.java
@@ -0,0 +1,13 @@
+package com.sandeep.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro3Application.class, args);
+	}
+
+}
